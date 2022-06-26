4eff7e2842b104787e9ba4232b62c57c13211a39
Dmitri Rezchikov <drezchikov@gmail.com>
2022-06-26 22:38:28 +0300

    Домашка

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -14,19 +15,25 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(title: const Text('Sight List Screen')),
-      // body: const Center(
-      //   child: Text(
-      //     "Sight List Screen",
-      //     style: TextStyle(
-      //       color: Colors.black,
-      //       fontSize: 40.0,
-      //     ),
-      //   ),
+
+      // body: Center(
+      //   child: Text("Hello!"),
       // ),
 
-      body: Center(
-        child: Text("Hello!"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: <Widget>[
+          Text("Hello!"),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Если кликнуть, появится клавиатура',
+            ),
+          ),
+        ],
       ),
     );
   }
