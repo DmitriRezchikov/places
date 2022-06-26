67f4d0311c07ef5bc2d72f8d589fcf150f793380
Dmitri Rezchikov <drezchikov@gmail.com>
2022-06-26 21:59:00 +0300

    some minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Some messages text',
+              'Places home page',
             ),
             // Text(
             //   '$_counter',
